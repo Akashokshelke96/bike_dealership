9942180bf911b33d659ae8f3b2d84aecbe4443af
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-27 21:52:06 +0530

    Car dealership project dealership

---
+++ src/udemy/dealership/Dealership.java
@@ -0,0 +1,17 @@
+package udemy.dealership;
+
+public class Dealership {
+    public static void main(String[] args) {
+        Customer cust1 = new Customer();
+        cust1.setName("AShok");
+        cust1.setAddress("143 Standlone Dist");
+        cust1.setCashOnHand(99000);
+
+
+        Vehicle vehicle = new Vehicle();
+
+
+        Employee emp = new Employee();
+
+    }
+}
